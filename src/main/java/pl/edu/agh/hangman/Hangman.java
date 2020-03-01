ed7aca5b4df7732d9dd4802b5d9452658af4b26b
basj2 <bajanik@student.agh.edu.pl>
2020-03-01 15:17:25 +0100

    Underscore to letter

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -60,8 +60,8 @@ public class Hangman {
 
     public static void main(String[] args) throws FileNotFoundException {
         Guess letterToGuess = new Guess();
-        //String letter = letterToGuess.guessLetter();
-        //System.out.println(letter); 
+        String letter = letterToGuess.guessLetter();
+        System.out.println(letter); 
 
     	WordsReader wordsReader = new WordsReader();
     	ArrayList<String> wordsList = wordsReader.getWordsList("src/main/resources/slowa.txt");
